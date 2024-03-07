9bdc8b368f89d1050d4339d9ea35a6e7dc8e0bbb
shaikh-vahab <shaikhvahab97@gmail.com>
2024-03-07 14:29:00 +0530

    New Update

--- frontend/src/components/Qualities.jsx
+++ frontend/src/components/Qualities.jsx
@@ -11,7 +11,6 @@ const Qualities = () => {
           <div class="">
             <img src="https://media.istockphoto.com/id/1159751783/photo/organic-apricot-fruit-and-juice.jpg?s=612x612&w=0&k=20&c=_Y11iX3h0WOdEea0AYPihdBrd6gPXglDFJKkj4zEGew=" alt="Image 1" width={'150px'}/>
             <img src="Carousel-2.jpeg" alt="Image 2"   width={'220px'}/>
-            {/* <img src="Carousel-3.jpeg" alt="Image 3"  width={'200px'} /> */}
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgzHkIIA3nDmex7oPRSnb5X-I2bCEKeJWtXA&usqp=CAU" alt="Image 2"   width={'300px'}/>
             <img src="Carousel-4.jpeg" alt="Image 4"   width={'220px'}/>
             <img src="Carousel-3.jpeg" alt="Image 3"  width={'150px'} />
@@ -24,4 +23,4 @@ const Qualities = () => {
   )
 }
 
-export default Qualities
+export default Qualities;
