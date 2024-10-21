077b1f7928bedd2e57fd24f42b675dfcc817f758
shaikh-vahab <shaikhvahab97@gmail.com>
2024-10-21 15:41:49 +0530

    first commit1

--- src/components/Juice.jsx
+++ src/components/Juice.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../Css/juices.css';
+import '../../src/Css/Juices.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
